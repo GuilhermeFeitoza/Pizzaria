f91888de151715655c1e078e4c3ca629358f9f6c
Guilherme Feitoza <guilhermefeitoza09@outlook.com>
2021-06-16 19:10:43 -0300

    escopo inicial das versões 1 e 2 e comandos sql de criação das tabelas

---
+++ pizzariaDML.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE Pizzaria
+GO 
+USE Pizzaria
+GO 
+CREATE TABLE tbCliente(
+	IdCliente int identity primary key,
+	Nome varchar(200),
+	Cpf varchar(11),
+	Email varchar(30),
+	Telefone varchar(12),
+	DataNascimento datetime,
+	Ativo char(1),
+);
+
+CREATE TABLE tbFuncionario(
+	Id int Identity primary key ,
+	Nome varchar(200),
+	Usuario varchar(30),
+	Senha varchar(200),
+	DataAdmissao datetime,
+	Ativo char(1),
+);
+
+CREATE TABLE tbIngredientes(
+	IdIngrediente int identity primary key ,
+	Nome varchar(30),
+	Descricao varchar(20),
+	QtdCal int,
+	  )
+CREATE TABLE tbPizza(
+	IdPizza int identity primary key,
+	Nome varchar(20),
+	Preco decimal,
+	Tamanho int ,
+
+)
+CREATE TABLE tbUsuario(
+	IdUsuario int identity primary key,
+	IdCliente int foreign key (IdCliente) References tbCliente,
+	Nome varchar(20),
+	Senha varchar(200),
+	Ativo char(1)
+)
+
+CREATE TABLE tbAvaliacao(
+	IdAvaliacao int identity primary key,
+	Titulo varchar(30),
+	Comentario varchar(50),
+	Nota int,
+	Ativo char(1),
+)
