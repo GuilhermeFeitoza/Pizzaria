294bf14ce63ce7765a382c6991af53081475ad8c
Guilherme Dionizio Feitoza <guilhermefeitoza09@outlook.com>
2022-08-15 23:13:41 -0300

    Mudança de layout do menu

--- desktop/src/App.vue
+++ desktop/src/App.vue
@@ -1,36 +1,42 @@
 <template>
   <div id="app">
-<header>
-    <NavBar/>
-</header>
-<br>
+    <header>
+      <NavBar />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <MenuBar />
 
-      <br>
-      <br>
-      <br>
-    <router-view/>
-    <Cliente-modal/>
-    <Funcionario-modal/>
 
+    </header>
+    <br />
+
+    <br />
+    <br />
+    <br />
+    <router-view />
+    <Cliente-modal />
+    <Funcionario-modal />
   </div>
 </template>
 <script>
-import NavBar from './components/NavBar.vue'
-import Home from './components/Home.vue'
-import ClienteModal from './components/modal/Cliente-modal.vue'
+import NavBar from "./components/NavBar.vue";
+import Home from "./components/Home.vue";
+import ClienteModal from "./components/modal/Cliente-modal.vue";
 
-import '../src/style.css'
-import FuncionarioModal from './components/modal/Funcionario-modal.vue'
-
+import "../src/style.css";
+import FuncionarioModal from "./components/modal/Funcionario-modal.vue";
+import MenuBar from "./components/MenuBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NavBar,
     Home,
     ClienteModal,
     FuncionarioModal,
-    
-   }
-}
+    MenuBar,
+  },
+};
 </script>
---
+++ desktop/src/components/MenuBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="margin: auto">
+    <div style="width: auto">
+      <span class="dot"
+        ><router-link to="/funcionarios" class="menu-text">
+          Funcionários
+        </router-link></span
+      >
+      <span class="dot"
+        ><router-link to="/cardapio" class="menu-text">
+          Cardapio
+        </router-link></span
+      >
+
+      <span class="dot"><a class="menu-text"> Avaliações </a></span>
+      <span class="dot">
+        <router-link class="menu-text" to="/clientes">
+          Clientes
+        </router-link></span
+      >
+      <span class="dot">
+        <router-link to="" class="menu-text"> Pedidos </router-link></span
+      >
+    </div>
+
+    <!-- 
+
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
