9883a2345e56cd53442759d0c6418d3c9d305818
Guilherme Dionizio Feitoza <guilhermefeitoza09@outlook.com>
2022-08-18 00:12:37 -0300

    Ajustes no menu CSS

--- desktop/src/components/MenuBar.vue
+++ desktop/src/components/MenuBar.vue
@@ -1,33 +1,33 @@
 <template>
-  <div style="margin: auto">
+  <div style="margin: auto;padding-left: 5%;">
     <div style="width: auto">
-      <span class="dot"
+      <span class="menu-rounded imgFuncionarios"
         ><router-link to="/funcionarios" class="menu-text">
           Funcionários
         </router-link></span
       >
-      <span class="dot"
+      <span class="menu-rounded imgCardapio "
         ><router-link to="/cardapio" class="menu-text">
           Cardapio
         </router-link></span
       >
 
-      <span class="dot"><a class="menu-text"> Avaliações </a></span>
-      <span class="dot">
+      <span class="menu-rounded imgAvaliacao"><a class="menu-text"> Avaliações </a></span>
+      <span class="menu-rounded imgCliente">
         <router-link class="menu-text" to="/clientes">
           Clientes
         </router-link></span
       >
-      <span class="dot">
+      <span class="menu-rounded imgPedidos">
         <router-link to="" class="menu-text"> Pedidos </router-link></span
       >
     </div>
 
     <!-- 
 
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span> -->
+    <span class="menu-rounded"></span>
+    <span class="menu-rounded"></span>
+    <span class="menu-rounded"></span> -->
   </div>
 </template>
 
