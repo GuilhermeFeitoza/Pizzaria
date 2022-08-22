042c9183475ffbcc7249269d3bb64db006029897
Guilherme Dionizio Feitoza <guilhermefeitoza09@outlook.com>
2022-08-22 00:33:15 -0300

    ajustes no card de ultimos pedidos

--- desktop/src/components/partial/UltimosPedidos.vue
+++ desktop/src/components/partial/UltimosPedidos.vue
@@ -3,44 +3,26 @@
     <br />
 
     <h1 style="font-family: Calibri">Últimos pedidos</h1>
-    <div style="display: inline-flex; max-width: 70%">
-      <div
-        class="card"
-        v-for="pedido in pedidos"
-        :key="pedido.index"
-        style=""
-      >
-        <img id="imgpedido" :src="pedido.img" />
-        <ul v-for="item in itemsPedido" :key="item.id">
-          <li>{{ item.nome }}</li>
-        </ul>
-      </div>
+     <card-pedidos :status="'E'"></card-pedidos>
+     
     </div>
-  </div>
+
 </template>
 
 <script>
-export default {
- data() {
-    return {
-      itemsPedido: [
-        { id: 1, nome: "Camarao", preco: 13.0 },
-        { id: 2, nome: "Calabresa", preco: 13.0 },
-        { id: 3, nome: "Frango catupiry", preco: 13.0 },
-        { id: 4, nome: "Toscana" },
-        { id: 5, nome: "Teste" },
-      ],
-      pedidos: [{img:require("../../assets/imgs/brigadeiro.jpg")},
-        {img:require("../../assets/imgs/peperoni.jpg")},
-        {img:require("../../assets/imgs/manjericao.jpg")},
-        {img:require("../../assets/imgs/portuguesa.jpg")},
-      
-       
-       ],
-    }
-    },
-      
-};
+import CardPedidos from './card-pedidos.vue';
+export default{
+
+components:{
+CardPedidos
+
+
+
+}
+
+  
+}
+
 </script>
 
 <style>
---
+++ desktop/src/components/partial/card-pedidos.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div class="card-pedidos">
+        <span>
+         Terça-feira 20/05/2022 20:00
+
+        </span>
+        <span class="card-pedidos-type-text">
+        DELIVERY
+        <img class="icon-size detalhes-pedido" src="../../assets/icons/setabaixo.png">
+        </span>
+        <br>
+        <br>
+        <span class="card-pedidos-status-text">
+          <img class="icon-size" :src="require(`@/assets/icons/${imgStatus}`)">  
+          <span>Entregue</span>
+        </span>
+        <br>
+        <span>
+          R$50,00
+        </span>
+        
+
+    
+    </div>
+ 
+</template>
+
+<script>
+export default {
+props:{
+    status:'E'
+
+    },
+computed:{
+    imgStatus(){
+        if(this.status=='E'){
+
+            return 'check.png';
+
+        }
+
+
+    }
+
+
+
+}
+}
+</script>
+
+<style>
+
+</style>
