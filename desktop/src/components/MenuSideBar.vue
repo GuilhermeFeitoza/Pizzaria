12ad386f0629485ed21c2eb26d80158eadbd0a86
Guilherme Feitoza <guilhermefeitoza09@outlook.com>
2023-07-06 22:44:24 -0300

    Ajustes menu lateral

--- desktop/src/components/MenuSideBar.vue
+++ desktop/src/components/MenuSideBar.vue
@@ -1,43 +1,44 @@
 <template>
   <div id="mySidebar" class="sidebar">
-      <router-link to="/home" class="menu-text">
-      
+    <router-link to="/home">
+        <i class="bi bi-house"></i>
         <span>Home</span>
-      </router-link>
-       <router-link to="/funcionarios" class="menu-text">
-      
+    </router-link>
+    <router-link to="/funcionarios" class="menu-text">
+    <div style=" width: 10vw;">
+        <i class="bi bi-person-workspace"></i>
         <span>Funcionários</span>
-      </router-link>
-      <router-link to="/cardapio" class="menu-text">
-        
+    </div>
+    </router-link>
+    <router-link to="/cardapio" class="menu-text">
+        <i class="bi bi-card-heading"></i>
         <span class="text-menu">Cardapio</span>
-      </router-link>
+    </router-link>
 
-
-      <router-link to="/clientes">
-      
-        <span class="text-menu">Clientes</span>
-      </router-link>
-      <router-link to="/pedidos">
-       
-        <span class="text-menu">Pedidos</span>
-      </router-link>
-
-
+    <router-link to="/clientes">
+      <i class="bi bi-people-fill"></i>
+      <span class="text-menu">Clientes</span>
+    </router-link>
+    <router-link to="/pedidos">
+      <i class="bi bi-scooter"></i>
+      <span class="text-menu">Pedidos</span>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-   
-  },
+  methods: {},
 };
 </script>
 
 <style>
 #mySideBar {
-  width: 15vw;
+  width: 16vw;
   margin-left: 15vw;
+}
+.menu-text {
+  position: relative;
+  left: 0px;
 }
 </style>
